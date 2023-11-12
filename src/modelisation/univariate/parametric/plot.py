dd0dc8d4d77fa0dfa45d4eeba8c0b27bf377e2ef
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2023-11-12 20:20:58 +0100

    refactor: add plot_estimation (singular) function

--- src/modelisation/univariate/parametric/plot.py
+++ src/modelisation/univariate/parametric/plot.py
@@ -5,6 +5,20 @@ from matplotlib import pyplot as plt
 from src.modelisation.univariate.parametric.models import model_type
 
 
+def plot_estimation(
+    model: model_type,
+    model_name: str,
+    event_times: pd.Series | np.ndarray,
+    event_observed: pd.Series | np.ndarray,
+    plot_title: str,
+    plot_label: str,
+) -> None:
+    model.fit(event_times, event_observed)
+    model.plot_survival_function(plot_label)
+
+    plt.title(plot_title)
+
+
 def plot_estimations(
     models: dict[str, model_type],
     event_times: pd.Series | np.ndarray,
@@ -12,11 +26,9 @@ def plot_estimations(
     same_plot: bool = False,
 ) -> None:
     for model_name, model in models.items():
-        model.fit(event_times, event_observed)
-
-        model.plot_survival_function(label=f"Modèle {model_name}")
-
-        plt.title("Fonction de survie")
+        title = "Fonction de survie"
+        label = f"Modèle {model_name}"
+        plot_estimation(model, model_name, event_times, event_observed, title, label)
 
         if not same_plot:
             plt.show()
