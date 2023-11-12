6c5f53ee61aafaca295becb6583fae3ab97ff1ef
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2023-11-12 20:43:14 +0100

    refactor: use constants in plot function

--- src/modelisation/univariate/parametric/plot.py
+++ src/modelisation/univariate/parametric/plot.py
@@ -2,7 +2,11 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-from src.constants import survival_function_xlabel
+from src.constants import (
+    survival_function_title,
+    survival_function_xlabel,
+    survival_function_ylabel,
+)
 from src.modelisation.univariate.parametric.models import model_type
 
 
@@ -10,14 +14,14 @@ def plot_estimation(
     model: model_type,
     event_times: pd.Series | np.ndarray,
     event_observed: pd.Series | np.ndarray,
-    plot_title: str,
     plot_label: str,
 ) -> None:
     model.fit(event_times, event_observed)
     model.plot_survival_function(label=plot_label)
 
-    plt.title(plot_title)
+    plt.title(survival_function_title)
     plt.xlabel(survival_function_xlabel)
+    plt.ylabel(survival_function_ylabel)
 
 
 def plot_estimations(
@@ -27,9 +31,8 @@ def plot_estimations(
     same_plot: bool = False,
 ) -> None:
     for model_name, model in models.items():
-        title = "Fonction de survie"
         label = f"Modèle {model_name}"
-        plot_estimation(model, event_times, event_observed, title, label)
+        plot_estimation(model, event_times, event_observed, label)
 
         if not same_plot:
             plt.show()
