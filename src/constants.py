ffb010fa1c9a261e9808bb4e42981c26b17cc04e
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2023-11-12 20:42:27 +0100

    refactor: add title & ylabel constants

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,3 @@
+survival_function_title = "Probabilité que le projet n'ait pas encore été financé"
 survival_function_xlabel = "Nombre de jours écoulés depuis le lancement du projet"
+survival_function_ylabel = r"Fonction de survie $P(t > T)$"
