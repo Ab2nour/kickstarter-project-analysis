0f24bd2ea4f26e966c9a863342f3c03522f663ab
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2023-11-12 21:40:30 +0100

    refactor: use dicts for constants

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,12 @@
-survival_function_title = "Probabilité que le projet n'ait pas encore été financé"
-survival_function_xlabel = "Nombre de jours écoulés depuis le lancement du projet"
-survival_function_ylabel = r"Fonction de survie $P(t > T)$"
+survival = {
+    "function_title": "Probabilité que le projet n'ait pas encore été financé",
+    "xlabel": "Nombre de jours écoulés depuis le lancement du projet",
+    "ylabel": r"Fonction de survie $P(t > T)$",
+}
+
+hazard = {
+    "function_title": "Fonction de risque cumulé",
+    # todo: add interpretation for our problem
+    "xlabel": "Nombre de jours écoulés depuis le lancement du projet",
+    "ylabel": r"Fonction de risque",  # todo: add formula
+}
